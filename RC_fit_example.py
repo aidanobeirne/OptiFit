2dbab8ce7391fe31747e94d7b3ee9686b922b57f
aidanobeirne <aidano@stanford.edu>
2022-06-23 22:05:37 -0700

    big update

--- RC_fit_example.py
+++ RC_fit_example.py
@@ -1,83 +1,96 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from lmfit import Minimizer, Parameters, report_fit
-import tmm as tmm
-import sys
-sys.path.append('/Users/aidan/Documents/Python/class_lmfit_solcore/') ##### Set your path to the class file here
-from lmfit_function_solcore import *
-import glob
-from scipy import fftpack
-from scipy.signal import butter, lfilter
 import os
-rootdir = os.getcwd()
+import sys
+import time
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+from TransferMatrix import models as models
 
+parameters = {'axes.labelsize': 15,
+              'axes.titlesize': 25,
+              'figure.figsize' : (0.75*16,0.75*9)}
+plt.rcParams.update(parameters)
 
+def shift_correction_range(spectra, energies, e_min, e_max):
+    temp = []
+    idx_max = min(range(len(energies)), key=lambda i: abs(energies[i]-e_min))
+    idx_min = min(range(len(energies)), key=lambda i: abs(energies[i]-e_max))
+    for spec in spectra:
+        offset = np.mean(spec[idx_min:idx_max])
+        shift = 0 - offset
+        temp.append(np.array(spec)+shift)
+    return temp
 
-####### I needed to filter out some stuff in my raw data
-def butter_bandstop_filter(data, lowcut, highcut, fs, order):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    high = highcut / nyq
-    i, u = butter(order, [low, high], btype='bandstop')
-    y = lfilter(i, u, data)
-    return y
+file = open(r'TransferMatrix/test_data.pkl', 'rb')
+master_data = pickle.load(file)
+file.close()
 
-fs = np.around(1/abs(np.diff(energies).mean()))
-dat = np.load('test_dat.npz')['spec']
-filtered_dat = butter_bandstop_filter(dat,13.86,36.7,fs,2)[141:]
-energies = np.load('test_dat.npz')['energies'][141:]
-plt.close('all')
+identifier = '1LBP RC doping'
+sorter = 'n'
+energies = 1240/np.array(master_data[identifier]['Em_range'][0])
+spectra = shift_correction_range(master_data[identifier]['RC'], energies, 1.372, 1.38)
 
-# # RC Fitting Using the LMFIT + Solcore modules
-#  - Add however many peaks (Gaussian or Lorentzian) you expect in the dielectric function. Each peak should contain 'pk' in the name (first argument of the "add_lorentzian" method).
-#  
-#  - Each peak addition in the "add_lorentzian" method contains 3 lists that represent the fit parameters defined for the LMFIT module.
-#  
-#  - Each fit parameter is defined by a list whose values are [name, value, vary (bool), min, max]. The parameter ordering is oscillator strength, energy resonance, and linewidth.
-#   
-#  - You can add layers with the "add_layer" method. The first parameter of the "add_layer" method is the layer thickness, and the second is the refractive index.
-#   
-#  - The sample layer must include 'sample' in the name. The refractive index used in the sample doesn't matter but should be set to 0 as a placeholder. If you want to include the energy dependent refractive index for a layer include '_full' at the end of the layer name and set the 'Vary' for the nlist to False.
+model = models.tmm_model(spectrum=spectra[50], energies=energies)
+model.add_lorentzian(name='pk1',
+                    amplitude=[2, True, 0, np.inf],
+                    resonant_energy=[1.7, True, 1.65, 1.75],
+                    broadening=[0.030, True, 0.005, 0.4])
+model.add_lorentzian('pk2',
+                    [0.3, True, 0.2, 1],
+                    [1.78, True, 1.77, 1.85],
+                    [0.040, True, 0.005, 0.055])
 
-
-model = tmm_model(Parameters(), filtered_dat, energies)
-model.add_lorentzian('pk1', model.energies,
-                    [0.26885770, True, 0, np.inf],
-                    [1.72186643, True, 0, 2],
-                    [0.08191602, True, 0.05, 0.25])
-model.add_lorentzian('pk2', model.energies,
-                    [0.06885770, True, 0, np.inf],
-                    [1.73186643, True, 1.72186643, 1.85],
-                    [0.03191602, True, 0.02, 0.25])
 
 ########## Add layers for TMM calculation
 model.add_layer('air',    [np.inf, False, 0, np.inf], [1, False, 0, np.inf, None])
-model.add_layer('tophbnfull',   [10.87, False, 9, 15],      [2, False, 0, 5.3, None, None])
-model.add_layer('sample', [0.3, False, 0.1, 2.7],     [1, True, 0, np.inf, None, None])
-model.add_layer('bottomhbnfull',   [13.5, False, 10, 25],      [2, False, 0, 5.3, None, None])
-model.add_layer('Grfull',   [1.96, False, 0.3, 2.1],    [1, False, 0, np.inf])
+model.add_layer('top_graphite_full',   [4.7, False, 0.3, 2.1],    [1, False, 0, np.inf])
+model.add_layer('top_hbn_full',   [18, False, 9, 20],      [3.9, False, 0, 5.3, None, None])
+model.add_layer('sample', [0.7, False, 0.1, 2.7],     [1, True, 0, np.inf, None, None])
+model.add_layer('bottom_hbn_full',   [13, False, 10, 25],      [3.9, False, 0, 5.3, None, None])
+model.add_layer('bottom_graphite_full',   [3.2, False, 0.3, 58.1],    [1, False, 0, np.inf])
 model.add_layer('quartz', [np.inf, False, 0, np.inf], [1.455, False, 1.440, 1.459, None, None])
+
 #### Background dielectric function (not including the +1 for epsilon(inf))
-model.params.add('eps_sample_r', 0, False, 2.0, 7)
+model.params.add('eps_sample_r', 0, False, 0, 7)
 model.params.add('eps_sample_r_slope', 0, False, -np.inf, np.inf)
-model.params.add('eps_sample_i', 0, False, -0.5, 0)
+model.params.add('eps_sample_i', 0, False, -0.5, 1)
 model.params.add('eps_sample_i_slope', 0, False, -np.inf, np.inf)
-#### Artifically add a linear background to the RC, could also fit to derivative
-model.params.add('offset', 0.1, True, -np.inf, np.inf)
+model.params.add('offset', 0, True, -np.inf, np.inf)
 model.params.add('slope', 0, True, -np.inf, np.inf)
 
-model.params.pretty_print()
+# model.params.pretty_print()
+plt.close('all')
 plt.figure()
-plt.plot(model.energies, model.data)
+plt.plot(model.energies, model.spectrum)
 plt.plot(model.energies, model.calc_rc())
 plt.title('Measured spectrum and initial guess');
 
-########################################## Fit RC with initial guesses
-model.ls_fit()
-plt.close('all')
-plt.figure()
-plt.plot(model.energies, model.data, 'k+')
-plt.plot(model.energies, model.calc_rc(), 'r')
-# Report fit results
-report_fit(model.result)
 
+model.fit()
+plt.figure()
+plt.plot(model.energies, model.spectrum, 'k+')
+plt.plot(model.energies, model.calc_rc(), 'r')
+
+
+# result_spec = model.calc_rc_resolved()
+# fig, ax = plt.subplots(3,1)
+# ax[0].plot(model.energies, model.data, 'k+')
+# ax[0].plot(model.energies, result_spec['total'], 'r')
+# ax[0].set_title('Monolayer RC Fit example')
+# ax[0].set_ylabel('$\Delta R/R$')
+# ax[1].plot(model.energies, model.data, 'k+')
+# ax[1].plot(model.energies, result_spec['pk1'], label = 'pk1')
+# ax[1].plot(model.energies, result_spec['pk2'], label = 'pk2');
+# # ax[1].plot(model.energies, result_spec['pk3'], label = 'pk3');
+# ax[1].legend()
+# ax[2].set_xlabel('Energy (eV)')
+# ax[1].set_ylabel('$\Delta R/R$');
+# eps = model.calc_n_sample(model.energies)**2
+# ax[2].plot(model.energies, np.imag(eps), color = 'C4', label = '$\epsilon_2$ of fit')
+# ax[2].legend()
+# # ax[1].plot(model.energies,result_spec[3], '--');
+# # Report fit results
+# report_fit(model.result)
+# # print('Fit Time = '+ str(tf-t0))
+# abs((model.result.residual)).mean()
--- TransferMatrix/models.py
+++ TransferMatrix/models.py
@@ -4,318 +4,145 @@ import numpy as np
 import csv
 from solcore.absorption_calculator.tmm_core_vec import coh_tmm
 import os
-cwd = os.path.dirname(os.path.realpath(__file__))
 
 
-##### To do:
+def interpolate_domain(known_yvals, known_domain, desired_domain):
+	known_domain = np.array(known_domain)
+	desired_domain = np.array(desired_domain)
+	minidx = np.where(known_domain == known_domain[known_domain < np.min(desired_domain)].max())[0][0]
+	maxidx = np.where(known_domain == known_domain[known_domain > np.max(desired_domain)].min())[0][0] +1
+	known_yvals = known_yvals[minidx:maxidx]
+	known_domain = known_domain[minidx:maxidx]
+	return np.interp(desired_domain, known_domain, known_yvals)
 
-def interpolate_domain(rinfo, exp):
-	known = np.array(rinfo[0])
-	exp = np.array(exp)
-	minidx = np.where(known == known[known < np.min(exp)].max())[0][0]
-	maxidx = np.where(known == known[known > np.max(exp)].min())[0][0] +1
-	e = known[minidx: maxidx]
-	n = np.array(rinfo[1][minidx : maxidx])
-	if len(rinfo) == 3:
-		k = np.array(rinfo[2][minidx : maxidx])
-	else:
-		k = 0
-		return np.interp(exp, e, n + 1j*k)
-
-
-class pl_model(Parameters):
-
-	def __init__(self, params = None, data = None, energies = None):
-		super(pl_model).__init__()
-		self.params = params
-		self.data = data
+class tmm_model:
+	def __init__(self, spectrum, energies):
+		self.spectrum = spectrum
 		self.energies = energies
-
-	def lorentz(self, x, a, x0, w):
-		# lor = a/(1+(x-x0)**2/(w**2)
-		lor = a/np.pi*(w/2)/((x-x0)**2+((w/2)**2))
-		return lor
-
-	def gaussian(self, x, a, x0, w):
-		gau = a*np.exp(-np.power(x - x0, 2.) / (2 * np.power(w, 2.)))
-		#gau = 1/(w*np.sqrt(2*np.pi))* np.exp(-(x-x0)**2/(2*w**2))
-		return gau
-
-	def add_lorentzian(self, name, x, a, x0, w):
-		self.params.add(name + '_l_a', *a)
-		self.params.add(name + '_l_x0', *x0)
-		self.params.add(name + '_l_w', *w)
-
-	def add_gaussian(self, name, x, a, x0, w):
-		self.params.add(name + '_g_a', *a)
-		self.params.add(name + '_g_x0', *x0)
-		self.params.add(name + '_g_w', *w)
-
-	def calc_spec(self):
-		spectrum = 0
-		pklist = []
-		for paramset in self.params.valuesdict():
-			pklist.append(paramset[:5])
-		pklist = list(set(pklist))
-		for pk in pklist:
-			if '_l' in pk:	
-				spectrum += self.lorentz(self.energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])
-			if '_g' in pk:
-				spectrum += self.gaussian(self.energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])
-		return spectrum + self.params['offset'] + self.params['slope']*self.energies + self.params['slope']*(self.energies - min(self.energies))
-
-	def plot_spec(self):
-		spectrum = 0
-		pklist = []
-		for paramset in self.params.valuesdict():
-			pklist.append(paramset[:5])
-		pklist = list(set(pklist))
-		plt.plot(self.energies, self.data, 'k+')
-		for pk in pklist:
-			if '_l' in pk:	
-				plt.plot(self.energies, self.lorentz(self.energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])+ self.params['offset'] + self.params['slope']*self.energies, '--')
-				spectrum += self.lorentz(self.energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])
-			if '_g' in pk:
-				spectrum += self.gaussian(self.energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])
-		plt.plot(self.energies, spectrum + self.params['offset'] + self.params['slope']*self.energies, 'r')
-
-	def loss(self, p):
-		self.params = p
-		return self.calc_spec()-self.data 
-
-	def fit(self):
-		A = Minimizer(self.loss, self.params)
-		self.result = A.minimize()
-		self.params = self.result.params
-
-
-class tmm_model(Parameters):
-
-	def __init__(self, params = None, data = None, energies = None):
-		super(tmm_model).__init__()
-		self.params = params
-		self.data = data
-		self.energies = energies
-		
-	def lorentz(self, x, a, x0, w):
+		self.params = Parameters()
+		self.layers = {}
+		self.peaks = []
+	
+	def complex_lorentzian(self, x, a, x0, w):
 		denominator=(x0**2-x**2)**2+(x**2*w**2)
 		real=a*(x0**2-x**2)/denominator
 		imag=x*w*a/denominator
 		lor=real+1j*imag
 		return lor
 
-	def linear_background(self, m, b, w):
-		return m*w + b
+	def add_lorentzian(self, name, amplitude, resonant_energy, broadening):
+		self.params.add('{}_a'.format(name), *amplitude)
+		self.params.add('{}_x0'.format(name), *resonant_energy)
+		self.params.add('{}_w'.format(name), *broadening)
+		self.peaks.append(name) # store the peaks in an instance attribute for easier access later
+		self.verify_params()
 
-	def	add_lorentzian(self, name, x, a, x0, w):
-		self.params.add(name + '_a', *a)
-		self.params.add(name + '_x0', *x0)
-		self.params.add(name + '_w', *w)
+	def import_rinfo(self, path):
+		with open(path, 'r') as f:
+			reader = csv.reader(f, delimiter=',')
+			headers = next(reader)
+			rinfo = np.array(list(reader)).astype(float).T
+			energies = rinfo[0]
+			n = rinfo[1]
+			try:
+				k = rinfo[2]
+				return energies, n, k
+			except IndexError:
+				return energies, n, 0*np.ones_like(n)
+			
 
 	def add_layer(self, name, d, n):
+		 # store the layers in an instance attribute for easier access later
 		self.params.add(name + '_d', *d)
 		self.params.add(name + '_n', *n)
-		if 'Gr' in name or 'gr' in name or 'GR' in name and 'full' in name:
-			with open(os.path.join(cwd,'graphenerefractiveindex.csv'), 'r') as f:
-				reader = csv.reader(f, delimiter=',')
-				headers = next(reader)
-				rinfo = np.array(list(reader)).astype(float).T
-			self.grfull = interpolate_domain(rinfo, self.energies)
-		if 'quartz' in name or 'sio2' in name or 'Sio2' in name or 'SiO2' in name and 'full' in name:
-			with open(os.path.join(cwd,'sio2refractiveindex.csv'), 'r') as f:
-				reader = csv.reader(f, delimiter=',')
-				headers = next(reader)
-				rinfo = np.array(list(reader)).astype(float).T
-			self.quartzfull = interpolate_domain(rinfo, self.energies)
-		if 'bare_si' in name and 'full' in name:
-			with open(os.path.join(cwd,'sirefractiveindex-extended.csv'), 'r') as f:
-				reader = csv.reader(f, delimiter=',')
-				headers = next(reader)
-				rinfo = np.array(list(reader)).astype(float).T
-			self.sifull = interpolate_domain(rinfo, self.energies)
-		# if 'hbn' in name or 'hBN' in name and 'full' in name:
-		# 	with open('/Users/aidan/Documents/Python/sio2refractiveindex.csv', 'r') as f:
-		# 		reader = csv.reader(f, delimiter=',')
-		# 		headers = next(reader)
-		# 		rinfo = np.array(list(reader)).astype(float).T
-		# 	self.quartzfull = interpolate_domain(rinfo, self.energies)
+		self.verify_params()
+		n = n[0] # grab the initial guess or, in the event that it wont be varied, the value given for the layer
+		d = d[0]
+
+		path = 'none'
+		if ('graphene' in name or 'graphite' in name) and ('full' in name):
+			path = os.path.join(os.path.dirname(__file__),'graphite_refractive_index_info-eV.csv')
+		elif ('quartz' in name or 'sio2' in name or 'Sio2' in name or 'SiO2' in name) and ('full' in name):
+			path = os.path.join(os.path.dirname(__file__),'quartz_refractive_index_info-eV.csv')
+		elif ('bare_si' in name or 'Si' in name or 'si' in name) and ('full' in name):
+			path = os.path.join(os.path.dirname(__file__),'silicon_refractive_index_info-eV.csv')
+		elif ('hbn' in name or 'hBN' in name) and ('full' in name):
+			path = 'custom'
+			n = 2.23 - 6.9e-4*1240/self.energies + 1j*0 # apparently in O.  Stenzel et al., Phys.  Status  Solidi  A (1996), but taken from Kim et al. JOSK 19, 503 (2015)
+
+		if 'csv' in path:
+			energies, nr, k = self.import_rinfo(path)
+			n_interpolated = interpolate_domain(nr, energies, self.energies)
+			k_interpolated = interpolate_domain(k, energies, self.energies)
+			n = n_interpolated + 1j*k_interpolated
+		elif 'custom' in path:
+			n=n
+		else:
+			if isinstance(n, complex):
+				n = np.ones_like(self.energies)*n
+			else:
+				n = np.ones_like(self.energies)*n +1j * np.zeros(np.shape(self.energies))
+		self.layers[name] = {'n': n, 'd': d} # store the interpolated domain in the layer dictionary to be called later
 
 	def get_nvals(self, includesample):
-		somelist = []
-		for a in self.params.valuesdict():
-			somelist.append(a)
-		nlist = []
-		for i in somelist:
-			if '_n' in i and i != 'baseline':
-				nlist.append(i)
-		nvals =[]
-		if includesample == True:
-			for n in nlist:
-				if 'sample' in n:
-					nvals.append(self.calc_n_sample(self.energies))
-				elif 'Gr' in n or 'gr' in n or 'GR' in n and 'full' in n:
-					nvals.append(self.grfull)
-				elif 'hbn' in n or 'hBN' in n and 'full' in n:
-					nvals.append(2.23 - 6.9e-4*1240/self.energies + 1j*0) # apparently in O.  Stenzel et al., Phys.  Status  Solidi  A (1996), but taken from Kim et al. JOSK 19, 503 (2015)
-				elif 'quartz' in n or 'sio2' in n or 'Sio2' in n or 'SiO2' in n and 'full' in n:
-					nvals.append(self.quartzfull)
-				elif 'bare_si' in n and 'full' in n:
-					nvals.append(self.sifull)
-				else:
-					nvals.append(np.ones_like(self.energies)*self.params.valuesdict()[n] +1j * np.zeros(np.shape(self.energies)))
-		if includesample == False:
-			for n in nlist:
-				if 'sample' in n:
-					pass
-				elif 'Gr' in n or 'gr' in n or 'GR' in n and 'full' in n:
-					nvals.append(self.grfull)
-				elif 'hbn' in n or 'hBN' in n and 'full' in n:
-					nvals.append(2.23 - 6.9e-4*1240/self.energies+ 1j*0)
-				elif 'quartz' in n or 'sio2' in n or 'Sio2' in n or 'SiO2' in n and 'full' in n:
-					nvals.append(self.quartzfull)
-				elif 'bare_si' in n and 'full' in n:
-					nvals.append(self.sifull)
-				else:
-					nvals.append(np.ones_like(self.energies)*self.params.valuesdict()[n] +1j * np.zeros(np.shape(self.energies)))		
+		# Function retrieves the refractive indices, n, for every layer in the stack
+		nvals = []
+		for layer, values in self.layers.items():
+			if 'sample' in layer and includesample:
+				nvals.append(self.calc_n_sample())
+			elif 'sample' in layer and not includesample:
+				pass
+			else:
+				nvals.append(values['n'])
 		return nvals
 
 	def get_dvals(self, includesample):
+		# Function retrieves the refractive thicknesses, d, for every layer in the stack
 		dvals = []
-		for d in self.params.valuesdict():
-			# print(d)
-			if '_d' in d:
-				# print(d)
-				if includesample == True:
-					dvals.append(self.params.valuesdict()[d])
-				if includesample == False:
-					if 'sample' not in d:
-						dvals.append(self.params.valuesdict()[d])
+		for layer, values in self.layers.items():
+			if 'sample' in layer and not includesample:
+				continue
+			dvals.append(values['d'])
 		return dvals
 
-	def calc_n_sample(self, energies):   
-		somelist = []
-		for paramset in self.params.valuesdict():
-			somelist.append(paramset[:3])
-		pklist = []
-		for i in somelist:
-			if 'pk' in i:
-				pklist.append(i)
-		pkset = set(pklist)
-		pklist = list(pkset)
-		eps_sample = 1 + (self.params['eps_sample_r']+ self.energies*self.params['eps_sample_r_slope']) + 1j*(self.params['eps_sample_i']+ self.energies*self.params['eps_sample_i_slope']) #self.params['offset'] + self.params['slope']*self.energies+ 1j*self.energies*0
-		for pk in pklist:
-			eps_sample += self.lorentz(energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])
+	def calc_n_sample(self):  
+		try:
+			eps_sample = 1 + (self.params['eps_sample_r']+ self.energies*self.params['eps_sample_r_slope']) + 1j*(self.params['eps_sample_i']+ self.energies*self.params['eps_sample_i_slope'])
+		except KeyError:
+			eps_sample = 1
+
+		for peak in self.peaks:
+			eps_sample += self.complex_lorentzian(self.energies, self.params['{}_a'.format(peak)].value, self.params['{}_x0'.format(peak)].value, self.params['{}_w'.format(peak)].value)
 		n_sample = np.sqrt(eps_sample)
 		return n_sample
 
-	def calc_rc(self):
-		target = coh_tmm('s', self.get_nvals(includesample = True), self.get_dvals(includesample = True), 0 + 1j*0, 1240/self.energies)
-		ref = coh_tmm('s', self.get_nvals(includesample = False), self.get_dvals(includesample = False), 0 + 1j*0, 1240/self.energies)
-		return (target['R']-ref['R'])/ref['R'] + self.params['offset'] + self.params['slope']*(self.energies - min(self.energies))
-
-	def calc_rc_resolved(self):
-		RCdict = {}
-		RCdict['total'] = self.calc_rc()
-
-		######## Calculate RC for each peak in the model
-		somelist = []
-		for paramset in self.params.valuesdict():
-			somelist.append(paramset[:3])
-		pklist = []
-		for i in somelist:
-			if 'pk' in i:
-				pklist.append(i)
-		pkset = set(pklist)
-		pklist = list(pkset)
-		print(pkset)
-
-		ref = coh_tmm('s', self.get_nvals(includesample = False), self.get_dvals(includesample = False), 0 + 1j*0, 1240/self.energies)
-
-		for count, pk in enumerate(pklist):
-			# compute n_sample with only one peak
-			eps_sample = 1 + (self.params['eps_sample_r']+ self.energies*self.params['eps_sample_r_slope']) + 1j*(self.params['eps_sample_i']+ self.energies*self.params['eps_sample_i_slope']) #self.params['offset'] + self.params['slope']*self.energies+ 1j*self.energies*0
-			eps_sample += self.lorentz(self.energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])
-			n_sample = np.sqrt(eps_sample)
-
-			# rgenerate list of nvals
-			somelist = []
-			for a in self.params.valuesdict():
-				somelist.append(a)
-			nlist = []
-			for i in somelist:
-				if '_n' in i and i != 'baseline':
-					nlist.append(i)
-			nvals_sample =[]
-			for n in nlist:
-				if 'sample' in n:
-					nvals_sample.append(n_sample)
-				elif 'Gr' in n or 'gr' in n or 'GR' in n and 'full' in n:
-					nvals_sample.append(self.grfull)
-				elif 'hbn' in n or 'hBN' in n and 'full' in n:
-					nvals_sample.append(2.23 - 6.9e-4*1240/self.energies + 1j*0) # apparently in O.  Stenzel et al., Phys.  Status  Solidi  A (1996), but taken from Kim et al. JOSK 19, 503 (2015)
-				elif 'quartz' in n or 'sio2' in n or 'Sio2' in n or 'SiO2' in n and 'full' in n:
-					nvals_sample.append(self.quartzfull)
-				elif 'bare_si' in n and 'full' in n:
-					nvals_sample.append(self.sifull)
-				else:
-					nvals_sample.append(np.ones_like(self.energies)*self.params.valuesdict()[n] +1j * np.zeros(np.shape(self.energies)))
-
-			target = coh_tmm('s', nvals_sample, self.get_dvals(includesample = True), 0 + 1j*0, 1240/self.energies)
-			ref = coh_tmm('s', self.get_nvals(includesample = False), self.get_dvals(includesample = False), 0 + 1j*0, 1240/self.energies)
-			RCdict[pk] = (target['R']-ref['R'])/ref['R'] + self.params['offset']+self.params['slope']*(self.energies - min(self.energies))
-		return RCdict
-		#####################
-
-	def calc_epsilon(self):
-		somelist = []
-		for paramset in self.params.valuesdict():
-			somelist.append(paramset[:3])
-		pklist = []
-		for i in somelist:
-			if 'pk' in i:
-				pklist.append(i)
-		pkset = set(pklist)
-		pklist = list(pkset)
-		print(pkset)
-
-		for count, pk in enumerate(pklist):
-			# compute n_sample with only one peak
-			eps_sample = 1 + (self.params['eps_sample_r']+ self.energies*self.params['eps_sample_r_slope']) + 1j*(self.params['eps_sample_i']+ self.energies*self.params['eps_sample_i_slope']) #self.params['offset'] + self.params['slope']*self.energies+ 1j*self.energies*0
-			eps_sample += self.lorentz(self.energies, self.params.valuesdict()[pk+'_a'], self.params.valuesdict()[pk+'_x0'], self.params.valuesdict()[pk+'_w'])
-		return np.real(eps_sample), np.imag(eps_sample)
-
 	def calc_target(self):
 		target = coh_tmm('s', self.get_nvals(includesample = True), self.get_dvals(includesample = True), 0, 1240/self.energies)
-		return (target['R'])
+		return target['R']
 
-	def calc_ref(self):
+	def calc_reference(self):
 		ref = coh_tmm('s', self.get_nvals(includesample = False), self.get_dvals(includesample = False), 0, 1240/self.energies)
 		return ref['R'] 
 
-	def check_params(self):
-		pass			
-
-	def fit(self):
-		A = Minimizer(self.loss, self.params)
-		self.result = A.minimize()
-		self.params = self.result.params
-
-	def ls_fit(self):
-		A = Minimizer(self.loss, self.params)
-		self.result = A.leastsq(self.params)
-		self.params = self.result.params
+	def calc_rc(self):
+		try:
+			RC = (self.calc_target() - self.calc_reference())/ self.calc_reference() + self.params['offset'] + self.params['slope']*(self.energies - min(self.energies))
+		except KeyError:
+			RC = (self.calc_target() - self.calc_reference())/ self.calc_reference()
+		return RC
 
 	def loss(self, p):
 		self.params = p
-		return self.calc_rc()-self.data 
+		return self.calc_rc()-self.spectrum 
 
-	def diffloss(self, p):
-		self.params = p
-		return np.diff(self.calc_rc())-np.diff(self.data) 
-
-	def diff_fit(self):
-		A = Minimizer(self.diffloss, self.params)
-		self.result = A.leastsq(self.params)
-		self.params = self.result.params
-    	
-    	
+	def fit(self):
+		A = Minimizer(self.loss, self.params)
+		result = A.minimize()
+		self.params = result.params
+	
+	def verify_params(self):
+		for param in self.params:
+			if self.params[param].min <= self.params[param].value <= self.params[param].max:
+				pass
+			else:
+				raise ValueError('{} has an initial guess that is not within the bounds'.format(param))
