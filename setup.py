3de78464c01339d619daa19a9e2fff9055540de0
aidanobeirne <aidano@stanford.edu>
2022-06-23 22:12:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Combining methods from solcore and Lmfit for an easy-to-use fitting procedure of thin film reflection contrast data',
     author='Aidan OBeirne',
     author_email='aidanobeirne@me.com',
-    url='https://github.com/aidanobeirne/LightWork.git',
+    url='https://github.com/aidanobeirne/TransferMatrix.git',
     packages=find_packages(),
     include_package_data=True,
     install_requires=['solcore', 'lmfit']
